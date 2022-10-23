95e0840eed438d42262c367f04cf600a88152eef
joroya.hms <47667368+joshuacba08@users.noreply.github.com>
2022-10-22 21:21:13 -0300

    inicio de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="w-screen h-screen bg-black p-4">
+        <button 
+          className='text-white py-2 px-3 bg-gradient-to-r from-sky-500 to-fuchsia-500 rounded active:scale-95 transition-all hover:brightness-110'
+        >
+          Click Here!
+        </button>
+    </div>
+  )
+}
+
+export default App
